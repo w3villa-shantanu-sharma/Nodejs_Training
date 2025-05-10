01630fef6c6df4fea09a4c0f17c06ef7bb34b892
w3villa-shantanu-sharma <shantanu.sharma@w3villa.com>
2025-05-10 10:38:06 +0530

    Email-verificaton done

--- Express_server/controllers/authControllers.js
+++ Express_server/controllers/authControllers.js
@@ -1,9 +1,13 @@
 const db = require('../DB/dbconfig');
 const bcrypt = require('bcrypt');
 const sendEmailVerification = require('../utils/sendEmailVerification');
+const jwt = require('jsonwebtoken');
+
 
 const Messages = require('../constants/messages');
 const StatusCodes = require('../constants/statusCode');
+const { secret, expiresIn } = require('../config/jwt');
+
 exports.registerUser = async (req, res) => {
 
     const { name, email, password } = req.body;
@@ -19,16 +23,26 @@ exports.registerUser = async (req, res) => {
 
     if (existed.length) return res.status(StatusCodes.BAD_REQUEST).json({ message: Messages.ERROR.EMAIL_ALREADY_REGISTERED });
 
+    const [existingTemp] = await db.query("SELECT * FROM email_verifications WHERE email = ?", [email]);
+    if (existingTemp.length) {
+      return res.status(400).json({ message: "Verification already sent. Check your inbox." });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
+    const token = jwt.sign({ email }, secret, { expiresIn });
+
 
     // Insert new user
-    const [result] = await db.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword]);
+    const [result] = await db.query(
+        "INSERT INTO email_verifications (name, email, password, token) VALUES (?, ?, ?, ?)",
+        [name, email, hashedPassword, token]
+      );
 
     console.log(result);
     
 
-    const newUser = { id: result.insertId, email };
-    await sendEmailVerification(newUser);
+    const newUser = { id: result.insertId, email  , token};
+    await sendEmailVerification(email , token);
 
     res.status(201).json({ message: Messages.SUCCESS.USER_REGISTERED });
 
@@ -42,18 +56,29 @@ exports.verifyEmail = async (req, res) => {
     try {
         // Verify token
         const decoded = jwt.verify(token, secret);
+        const email  = decoded.email;
 
         // Check if token exists in DB
         const [rows] = await db.query('SELECT * FROM email_verifications WHERE token = ?', [token]);
         if (rows.length === 0) return res.status(StatusCodes.BAD_REQUEST).json({ message: Messages.ERROR.INVALID_OR_EXPIRED_TOKEN });
 
-        const userId = decoded.userId;
+
+        const user = rows[0];
+
+        // Move to users table
+        await db.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [
+          user.name,
+          user.email,
+          user.password
+        ]);
 
         // Update user as verified
-        await db.query('UPDATE users SET isVerified = true WHERE id = ?', [userId]);
+        // await db.query('UPDATE users SET isVerified = true WHERE id = ?', [userId]);
 
         // Delete verification token
-        await db.query('DELETE FROM email_verifications WHERE user_id = ?', [userId]);
+        // await db.query('DELETE FROM email_verifications WHERE user_id = ?', [userId]);
+        await db.query("DELETE FROM email_verifications WHERE email = ?", [email]);
+
 
         res.json({ message: Messages.SUCCESS.EMAIL_VERIFIED });
     } catch (err) {
