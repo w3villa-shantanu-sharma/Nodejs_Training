8ea93a95644694d6e7bfd0cdb0ff272170acca0a
w3villa-shantanu-sharma <shantanu.sharma@w3villa.com>
2025-08-07 11:38:27 +0530

    Refactors server code to use ES6 modules and adds YouTube routes
    Converts the server code to use ES6 import syntax for better readability and maintainability.
    
    Adds a new route for YouTube functionalities, enhancing the API capabilities.
    
    Updates configuration loading to use a relative path for the environment variables.

--- Express_server/index.js
+++ Express_server/index.js
@@ -1,20 +1,27 @@
-const express = require('express');
-const passport = require('passport');
-const cors = require('cors');
-require('dotenv').config({ path: __dirname + '/.env' });
-require('./config/passport');
-require('./utils/expirePlans');
+import dotenv from 'dotenv';
+import express from 'express';
+import passport from 'passport';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import './config/passport.js';
+import './utils/expirePlans.js';
+import './utils/clean-jobs.js';
 
-const userRouter = require('./routes/userRoutes');
-const googleAuthRoutes = require('./routes/googleAuthRoutes');
-const podcastRouter = require('./routes/podcast');
-const paymentRouter = require('./routes/payment');
-const errorMiddleware = require('./middlewares/errorMiddleware');
+import userRouter from './routes/userRoutes.js';
+import googleAuthRoutes from './routes/googleAuthRoutes.js';
+import podcastRouter from './routes/podcast.js';
+import paymentRouter from './routes/payment.js';
+import errorMiddleware from './middlewares/errorMiddleware.js';
+
+import youtubeRoutes from './routes/youtubeRoutes.js';
+
+
+dotenv.config({ path: './.env' });
 
 const app = express();
 const PORT = 4000;
 
-//  Enable CORS for your frontend origin
+// Enable CORS for your frontend origin
 app.use(cors({
   origin: [
     'http://localhost:5173',
@@ -24,27 +31,31 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(passport.initialize());
 
-//  Optional: Request logging
+// Optional: Request logging
 app.use((req, res, next) => {
   console.log(`${req.method} request to ${req.url}`);
   next();
 });
 
-//  Routes
+// Routes
 app.use('/api/users', userRouter);
 app.use('/api/users/auth', googleAuthRoutes);
 app.use('/api/podcast', podcastRouter);
 app.use('/api/payment', paymentRouter);
+app.use('/api/youtube', youtubeRoutes);
+
+
 
 app.use(errorMiddleware);
 
-//  Start server
+// Start server
 app.listen(PORT, (error) => {
   if (!error) {
-    console.log(" Server is running on port:", PORT);
+    console.log("Server is running on port:", PORT);
   } else {
-    console.error(" Failed to start server:", error);
+    console.error("Failed to start server:", error);
   }
 });
